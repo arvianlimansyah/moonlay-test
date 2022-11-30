e997b829ed305d4b1f8fce402bf40b4cd379b8fe
arvianlimansyah <arvianlimansyah@gmail.com>
2022-11-30 20:02:43 +0700

    init repo moonlay test

---
+++ server/init.go
@@ -0,0 +1,16 @@
+package server
+
+import "fmt"
+
+// "golang-template/app/routes"
+// "golang-template/app/utils"
+
+// "github.com/joho/godotenv"
+
+func Run() {
+	// godotenv.Load()
+	// utils.InitLogger()
+	// routes.Run()
+
+	fmt.Println("Test")
+}
